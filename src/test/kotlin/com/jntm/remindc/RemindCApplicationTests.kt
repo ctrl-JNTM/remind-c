f16b5e47991c9bb6807670f296ac5a1c79f6d0cf
chengang <gang.chen@excarx.com.cn>
2019-11-18 02:36:45 -0800

    初始化jenkinsfile

--- src/test/kotlin/com/jntm/remindc/RemindCApplicationTests.kt
+++ src/test/kotlin/com/jntm/remindc/RemindCApplicationTests.kt
@@ -1,10 +1,16 @@
 package com.jntm.remindc
 
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class RemindCApplicationTests {
+
+    @BeforeAll
+    fun beforeAll(){
+
+    }
 
     @Test
     fun contextLoads() {
